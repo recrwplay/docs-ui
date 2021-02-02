d516fc1367426a543561bf5caf8767f1d8c4203b
Neil Dewhurst <neil.dewhurst@neo4j.com>
2021-02-02 10:16:07 +0000

    code change

--- src/js/06-code.js
+++ src/js/06-code.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var language = block.hasAttribute('class') &&
       block.getAttribute('class').match(/language-([a-z0-9-])+/i)[0].replace('language-', '')
 
-    if (language && ignore.indexOf(language.toLowerCase()) > -1) return  
+    if (language && ignore.indexOf(language.toLowerCase()) > -1) return
 
     var languageDiv = document.createElement('div')
     languageDiv.className = 'code-language'
